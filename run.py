6a4492e02d827f92415a5430405ff94be29e2cf2
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-20 19:44:59 +0000

    cloned taskmanager over from other repo

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
-from taskmanager import app
-
+from taskmanager import app, db
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Ensure this is within the application context
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        debug=os.environ.get("DEBUG") == 'True'
     )
